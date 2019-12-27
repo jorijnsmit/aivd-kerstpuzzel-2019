7550c74c30ba9278ce162a08b444700732a31e4d
Jorijn Jacko Smit <github@jorijnsmit.com>
2019-12-27 14:46:55 +0100

    added checksum function, renamed to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def run_tournament(bids):
     scores = [0 for i in range(len(bids))]
@@ -21,6 +22,13 @@ def run_tournament(bids):
     for i in range(min(5, len(bids))):
         print (bids[i], scores[i])
 
+def checksum(bids):
+    df = pd.DataFrame(bids)
+    assert df.sum(axis=1).max() == df.sum(axis=1).min() == 100
+    assert len(df.columns) == 10
+    assert df.min().min() >= 0
+    return True
+
 egbids = []
 egbids.append([10, 10, 10, 10, 10, 10, 10, 10, 10, 10])
 egbids.append([1, 1, 3, 4, 7, 9, 13, 16, 21, 25])
@@ -32,18 +40,5 @@ egbids.append([1, 11, 11, 11, 11, 11, 11, 11, 11, 11])
 egbids.append([25, 0, 0, 0, 0, 0, 0, 0, 25, 25, 25])
 egbids.append([1, 3, 5, 7, 9, 11, 13, 15, 17, 19])
 
-def mutate_bid(li):
-    new_li = []
-    for bid in li:
-        new_bid = bid + np.random.randint(-1, 1)
-        new_li.append(new_bid)
-    return new_li
-
+checksum(egbids)
 run_tournament(egbids)
-
-# test mutator
-mutate_bid(egbids[3])
-
-# todo
-# make checksum function that makes sure sum == 100
-#                             makes sure bid >= 0
