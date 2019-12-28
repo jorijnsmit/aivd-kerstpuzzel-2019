947b4c67b89b96d86167574fb642c42907404594
Jorijn Jacko Smit <github@jorijnsmit.com>
2019-12-28 01:51:17 +0100

    better population control. new loop but relies too much on pandas still.

--- main.py
+++ main.py
@@ -37,3 +37,19 @@ for n in range(1000):
         print(f'\nnew champion in round {n}: {CHAMPION}')
     for i, _ in enumerate(POPULATION):
         POPULATION[i] = mutate(POPULATION[i], redist=0.05)
+
+def population_loop():
+    # sloooooooow
+    # nog teveel computaties in pandas ipv numpy
+    df = pd.DataFrame(random_population(100, 10, 100))
+    #df = df.append([[2, 2, 2, 2, 2, 2, 12, 22, 27, 27]])
+    df.columns = ['gene 1', 'gene 2', 'gene 3', 'gene 4', 'gene 5', 'gene 6', 'gene 7', 'gene 8', 'gene 9', 'gene 10']
+
+    for n in range(1000):
+        df['fitness'] = df.apply(calc_fitness, args=(df, ), axis=1, raw=True)
+        df = selection(df)
+        df.pop('fitness')
+        print(n, df.iloc[0].tolist())
+        df = df.append(df.apply(mutate_simple, axis=1, raw=True), ignore_index=True)
+
+    plot_individual(df.iloc[0,:10])
