5e5543e81570d4fbdcb0f430f9183a078ebfb0d9
Jorijn Jacko Smit <github@jorijnsmit.com>
2019-12-28 21:24:18 +0100

    parent stays alive so increased chances for mutation

--- genetic_algorithms.py
+++ genetic_algorithms.py
@@ -11,8 +11,6 @@ def redist_points(bid):
             bid[(env + 9) % 10] += 1
         else:
             bid[(env + 1) % 10] += 1
-    assert bid.sum() == 100
-    assert bid.min() >= 0
     return bid
 
 
@@ -21,29 +19,25 @@ def shift_all(bid):
     if np.random.randint(0, 1) == 0:
         t = bid[0]
         for i in range(9):
-            bid[i] = bid[i+1]
+            bid[i] = bid[i + 1]
         bid[9] = t
     else:
         t = bid[9]
         for i in range(9):
-            bid[9-i] = bid[9-i-1]
+            bid[9 - i] = bid[9 - i - 1]
         bid[0] = t
-    assert bid.sum() == 100
-    assert bid.min() >= 0
     return bid
 
 def swap_values(bid):
     """randomly swap two adjacent envelopes"""
     i = np.random.randint(0, 9)
     t = bid[i]
-    bid[i] = bid[i+1]
-    bid[i+1] = t
-    assert bid.sum() == 100
-    assert bid.min() >= 0
+    bid[i] = bid[i + 1]
+    bid[i + 1] = t
     return bid
 
 
-def mutate(bid, redist=0.1, swap=0.1, shift=0.05):
+def mutate(bid, redist=0.5, swap=0.5, shift=0.1):
     """mutate a list of bids using given thresholds"""
     if np.random.random() < redist:
         bid = redist_points(bid)
